14e6f4fda5374014cf3f812ef1e89e0589ab0bae
BadWififi <a.m.crane@hotmail.com>
2025-09-04 18:17:49 +0930

    Welcome screen 04/09/25
    Added a welcome screen and instructions when you start the game

---
+++ GroupGame/Assets/Code/UIManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI Instructions;
+    [SerializeField] TextMeshProUGUI WelcomeText;
+    public Canvas LevelUI;
+
+    public bool GameStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelUI.enabled = true;
+        Instructions.enabled = false;
+        WelcomeText.enabled = true;
+        GameStarted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) && GameStarted == false)
+        {
+            WelcomeText.enabled = false;
+            Instructions.enabled = true;
+            GameStarted = true;
+        }
+        if (GameStarted == true)
+        {
+            Invoke("SetFalse", 5.0f); // disable after 5 secondse;
+        }
+    }
+    void SetFalse()
+    {
+        LevelUI.enabled = false;
+    }
+}
