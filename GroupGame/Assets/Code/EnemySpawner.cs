ec127c4e77d3173232dcdc70735ed9b960c09d17
notjt123 <114853397+notjt123@users.noreply.github.com>
2025-09-25 17:57:18 +0930

    Enemy Spawner
    Might need a little bug test

---
+++ GroupGame/Assets/Code/EnemySpawner.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    GameObject currentPoint;
+    int index;
+
+    public GameObject[] enemies;
+    public float minTimeBetweenSpawns;
+    public float maxTimeBetweenSpawns;
+    public bool canSpawn;
+    public float spawnTime;
+    public int enemiesInRoom;
+    public bool spawnerDone;
+    private void Start()
+    {
+        Invoke("SpawnEnemy", 0.5f);
+    }
+
+    private void Update()
+    {
+        if (canSpawn)
+        { 
+            spawnTime -= Time.deltaTime;
+            if (spawnTime < 0)
+            {
+                canSpawn = false;
+            }
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        index = Random.Range(0, spawnPoints.Length);
+        currentPoint = spawnPoints[index];
+        float timeBetweenSpawns = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+
+        if (canSpawn)
+        {
+            Instantiate(enemies[Random.Range(0, enemies.Length)], currentPoint.transform.position,Quaternion.identity);
+            enemiesInRoom++;
+        }
+
+        Invoke("SpawnEnemy", timeBetweenSpawns);
+        if(spawnerDone)
+        {
+            //DoneSpawning
+        }
+    }
+}
