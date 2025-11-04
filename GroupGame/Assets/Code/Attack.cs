003d7921ba705a23414df33bfbce5f5c5835716f
BadWififi <a.m.crane@hotmail.com>
2025-11-04 16:33:18 +1030

    Attack working (no bullet)

--- GroupGame/Assets/Code/Attack.cs
+++ GroupGame/Assets/Code/Attack.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
     public Transform firePoint;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            Shoot();
+            Debug.Log("You have shot a bullet");
+        }
+    }
+    void Shoot ()
+    {
+        // shooting code
     }
 }
